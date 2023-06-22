b89fa21772370ec7943e8137ab6e1c1ef5be2425
Dnyanesh Desai <dnyaneshdesai52@gmail.com>
2023-06-22 21:08:46 +0530

    java file version 1 for devops project

---
+++ demo.java
@@ -0,0 +1,16 @@
+public class FactorialExample {
+
+    public static void main(String[] args) {
+        int number = 5;
+        long factorial = calculateFactorial(number);
+        System.out.println("The factorial of " + number + " is: " + factorial);
+    }
+
+    public static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
